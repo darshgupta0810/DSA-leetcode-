f69bdc5c76fb3c0c3533f48f609f2d8da4950c00
darshgupta0810 <darshgupta005@gmail.com>
2026-07-26 14:55:19 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (38.47%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -1,50 +1,25 @@
 class Solution {
 public:
     bool isValid(string s) {
-    stack <char> st;
-    char p;
-    int q=0;
-    int m=0;
-    if(s[0]=='}' or s[0]==')' or s[0]==']'){
-      return false;
-    }
-    else{
-      pair <char,char> p1;
-      pair <char,char> p2;
-      pair <char,char> p3;
-      p1.first='{';
-      p1.second='}';
-      p2.first='(';
-      p2.second=')';
-      p3.first='[';
-      p3.second=']';
-      for(int i=0;i<s.length();i++){
-        if(s[i]=='{' or s[i]=='(' or s[i]=='['){
-          st.push(s[i]);
-          p=st.top();
-          cout<<p<<endl;
+        stack <char> st;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='{' or s[i]=='[' or s[i]=='('){
+                st.push(s[i]);
+            }
+            else{
+                if(st.empty()) return false;
+                else{
+                    char p=st.top();
+                    st.pop();
+                    if(s[i]=='}' and p!='{') return false;
+                    if(s[i]==')' and p!='(') return false;
+                    if(s[i]==']' and p!='[') return false;
+                }
+            }
         }
+        if(st.empty()) return true;
         else{
-          if(st.size()==0){
-            m=1;
-            break;
-          }
-          p=st.top();
-          if(p==p1.first and s[i]==p1.second or p==p2.first and s[i]==p2.second or p==p3.first and s[i]==p3.second){
-            st.pop();
-            continue;
-          }
-          else{
-            break;
-          }
+            return false;
         }
-      }
-      if(m==0 and st.size()==0){
-        return true;
-      }
-      else{
-        return false;
-      }
     }
-  }
 };
