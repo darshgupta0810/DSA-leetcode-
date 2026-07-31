03db4e5699aaae58d971034d9789717349f8fffb
darshgupta0810 <darshgupta005@gmail.com>
2026-07-31 09:54:40 +0530

    Time: 15 ms (69.25%) | Memory: 25.9 MB (59.38%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -1,13 +1,16 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-
+        if (strs.empty())
+            return {};
         vector<pair<string,string>> s;
+        int n=strs.size();
 
-        for (string str : strs) {
-            string key = str;
-            sort(key.begin(), key.end());
-            s.push_back({key, str});
+        for (int i=0;i<n;i++) {
+            string s1 = strs[i];
+            string s2 = strs[i];
+            sort(s1.begin(), s1.end());
+            s.push_back({s1, s2});
         }
 
         sort(s.begin(), s.end());
@@ -17,13 +20,13 @@ public:
         vector<string> curr;
         curr.push_back(s[0].second);
 
-        for (int i = 1; i < s.size(); i++) {
-            if (s[i].first == s[i-1].first)
-                curr.push_back(s[i].second);
+        for (int i = 0; i < s.size()-1; i++) {
+            if (s[i].first == s[i+1].first)
+                curr.push_back(s[i+1].second);
             else {
                 ans.push_back(curr);
                 curr.clear();
-                curr.push_back(s[i].second);
+                curr.push_back(s[i+1].second);
             }
         }
 
