6c901fe5bb0bdfdd5320563e476085eb806d381c
darshgupta0810 <darshgupta005@gmail.com>
2026-08-07 15:43:51 +0530

    Time: 7 ms (4.91%) | Memory: 25.9 MB (93.85%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,43 +1,39 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-      if(height.size()==1){
+        int n=height.size();
+        if(n==1){
             return 0;
         }
         else{
-            int n=height.size();
-            int l=height[0];
-            int r=height[n-1];
-            vector <int> v;
-            vector <int> p;
-            p.push_back(-1);
-            v.push_back(-1);
-            for(int i=1;i<n;i++){
-                if(height[i]>l){
-                    l=height[i];
-                    v.push_back(-1);
-                }
-                else{
-                    v.push_back(l);
-                }
-            }
-            for(int i=n-2;i>=0;i--){
-                if(height[i]>r){
-                    p.push_back(-1);
-                    r=height[i];
-                }
-                else{
-                    p.push_back(r);
-                }
-            }
-            reverse(p.begin(),p.end());
+            int l=0;
+            int r=n-1;
+            int lm=0;
+            int rm=0;
             int sum=0;
-            for(int i=0;i<n;i++){
-                if(v[i]!=-1 and p[i]!=-1){
-                sum=sum+min(v[i],p[i])-height[i];
+            while(l!=r){
+                if(height[l]<=height[r]){
+                    if(lm<height[l]){
+                        lm=height[l];
+                    }
+                    if(lm-height[l]>=0){
+                        sum=sum+lm-height[l];
+                    }
+                    l++;
                 }
+
+                else{
+                    if(rm<height[r]){
+                        rm=height[r];
+                    }
+                    if(rm-height[r]>=0){
+                        sum=sum+rm-height[r];
+                    }
+                    r--;
+                }
+
             }
             return sum;
-        }  
+        }
     }
 };
